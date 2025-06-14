9a287212719520816e495f9d3096b5bc3058cd01
murali <muralimittireddy1234@gmail.com>
2025-06-15 01:29:34 +0530

    initial commit

---
+++ src/main/java/com/murali/batch/spring_batch_data_processor/SpringBatchDataProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.murali.batch.spring_batch_data_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDataProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murali/batch/spring_batch_data_processor/SpringBatchDataProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murali.batch.spring_batch_data_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDataProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
